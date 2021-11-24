b8172906b4f2377c1b7f47bfd87769636d9f2578
StarArawn <toasterthegamer@gmail.com>
2021-11-24 15:00:13 -0500

    Delete old renderer.

--- bevy_kayak_ui/src/render/text/font.rs
+++
@@ -1,8 +0,0 @@
-use bevy::reflect::TypeUuid;
-use kayak_font::Font;
-
-#[derive(Debug, Clone, TypeUuid)]
-#[uuid = "4fe4732c-6731-49bb-bafc-4690d636b848"]
-pub struct KayakFont {
-    pub font: Font,
-}

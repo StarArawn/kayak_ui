db4f641fcde8d92a0a8965659ca5926be4fdcaf1
StarArawn <toasterthegamer@gmail.com>
2021-12-13 13:14:12 -0500

    Cleanup of cargo stuff.

--- src/lib.rs
+++ src/lib.rs
@@ -5,3 +5,13 @@ pub mod widgets {
 pub mod core {
     pub use kayak_core::*;
 }
+
+#[cfg(feature = "bevy_renderer")]
+pub mod font {
+    pub use kayak_font::*;
+}
+
+#[cfg(feature = "bevy_renderer")]
+pub mod bevy {
+    pub use bevy_kayak_ui::*;
+}

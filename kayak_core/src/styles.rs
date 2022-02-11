24ae21eef902aee5e437477a76e9099398e7f39c
MrGVSV <gino.valente.code@gmail.com>
2022-02-11 15:28:41 -0800

    Documented styles module and added tests

--- kayak_core/src/styles.rs
+++ kayak_core/src/styles.rs
@@ -1,8 +1,13 @@
+//! Contains code related to the styling of widgets
+
 pub use morphorm::{LayoutType, PositionType, Units};
 
 use crate::cursor::PointerEvents;
 use crate::{color::Color, render_command::RenderCommand, CursorIcon};
 
+/// The base container of all style properties
+///
+/// The default value for this enum is [`StyleProp::Unset`].
 #[derive(Debug, Clone, PartialEq)]
 pub enum StyleProp<T: Default + Clone> {
     /// This prop is unset, meaning its actual value is not determined until style resolution,
@@ -21,8 +26,8 @@ pub enum StyleProp<T: Default + Clone> {
 }
 
 impl<T> Default for StyleProp<T>
-where
-    T: Default + Clone,
+    where
+        T: Default + Clone,
 {
     fn default() -> Self {
         Self::Unset
@@ -30,9 +35,15 @@ where
 }
 
 impl<T> StyleProp<T>
-where
-    T: Default + Clone,
+    where
+        T: Default + Clone,
 {
+    /// Resolves this style property into a concrete value
+    ///
+    /// # Panics
+    ///
+    /// Panics if the style property has been set to [`StyleProp::Inherit`] since it needs to
+    /// first be assigned the value of its parent before being resolved.
     pub fn resolve(&self) -> T {
         match self {
             StyleProp::Unset => T::default(),
@@ -62,6 +73,9 @@ impl<T: Default + Clone> From<T> for StyleProp<T> {
     }
 }
 
+/// A macro that simply wraps the definition struct of [`Style`], allowing
+/// some methods to be automatically defined. Otherwise, there would be a _lot_ of
+/// copying and pasting, resulting in fragile code.
 macro_rules! define_styles {
     (
         // #[derive(...)]
@@ -134,35 +148,135 @@ macro_rules! define_styles {
 }
 
 define_styles! {
+
+    /// A struct used to define the look of a widget
+    ///
+    /// # Example
+    ///
+    /// All fields are `pub`, so you can simply define your styles like:
+    ///
+    /// ```
+    /// # use kayak_core::styles::{Style, StyleProp, Units};
+    /// let style = Style {
+    ///   width: StyleProp::Value(Units::Pixels(100.0)),
+    ///   // Or using `into()` to convert to `StyleProp`
+    ///   // width: Units::Pixels(100.0).into(),
+    ///   ..Default::default()
+    /// };
+    /// ```
+    ///
+    /// You can also create styles using other styles in a builder-like syntax:
+    ///
+    /// ```
+    /// # use kayak_core::styles::{Style, StyleProp, Units};
+    /// let style_a = Style {
+    ///   width: Units::Pixels(100.0).into(),
+    ///   ..Default::default()
+    /// };
+    /// let style_b = Style {
+    ///   height: Units::Pixels(100.0).into(),
+    ///   ..Default::default()
+    /// };
+    ///
+    /// let style = Style::default() // <- Initializes all fields as `StyleProp::Unset`
+    ///   // Applied first (sets any `StyleProp::Unset` fields)
+    ///   .with_style(&style_a)
+    ///   // Applied second (sets any remaining `StyleProp::Unset` fields)
+    ///   .with_style(&style_b);
+    /// ```
     #[derive(Debug, Default, Clone, PartialEq)]
     pub struct Style {
+        /// The background color of this widget
+        ///
+        /// Only applies to widgets marked [`RenderCommand::Quad`]
         pub background_color : StyleProp<Color>,
+        /// The color of the border around this widget
+        ///
+        /// Currently, this controls all border sides.
+        ///
+        /// Only applies to widgets marked [`RenderCommand::Quad`]
         pub border_color: StyleProp<Color>,
+        /// The radius of the corners (in pixels)
+        ///
+        /// The order is (Top, Right, Bottom, Left).
+        ///
+        /// Only applies to widgets marked [`RenderCommand::Quad`] and [`RenderCommand::Image`]
         pub border_radius: StyleProp<(f32, f32, f32, f32)>,
+        /// The widths of the borders (in pixels)
+        ///
+        /// The order is (Top, Right, Bottom, Left).
+        ///
+        /// Only applies to widgets marked [`RenderCommand::Quad`]
         pub border: StyleProp<(f32, f32, f32, f32)>,
+        /// The distance between the bottom edge of this widget and the bottom edge of its containing widget
         pub bottom: StyleProp<Units>,
+        /// The text color for this widget
+        ///
+        /// This property defaults to [`StyleProp::Inherit`] meaning that setting this field to some value will
+        /// cause all descendents to receive that value, up to the next set value.
+        ///
+        /// Only applies to widgets marked [`RenderCommand::Text`]
         pub color: StyleProp<Color>,
+        /// The cursor icon to display when hovering this widget
         pub cursor: StyleProp<CursorIcon>,
+        /// The height of this widget
         pub height: StyleProp<Units>,
+        /// The layout method for children of this widget
         pub layout_type: StyleProp<LayoutType>,
+        /// The distance between the left edge of this widget and the left edge of its containing widget
         pub left: StyleProp<Units>,
+        // TODO: Remove
         pub margin_bottom: StyleProp<Units>,
+        // TODO: Remove
         pub margin_left: StyleProp<Units>,
+        // TODO: Remove
         pub margin_right: StyleProp<Units>,
+        // TODO: Remove
         pub margin_top: StyleProp<Units>,
+        /// The maximum height of this widget
         pub max_height: StyleProp<Units>,
+        /// The maximum width of this widget
         pub max_width: StyleProp<Units>,
+        /// The minimum height of this widget
         pub min_height: StyleProp<Units>,
+        /// The minimum width of this widget
         pub min_width: StyleProp<Units>,
+        /// The inner padding between the bottom edge of this widget and its children
+        ///
+        /// A child with their own `bottom` property set to anything other than `Units::Auto` will
+        /// override the padding set by this widget
         pub padding_bottom: StyleProp<Units>,
+        /// The inner padding between the left edge of this widget and its children
+        ///
+        /// A child with their own `left` property set to anything other than `Units::Auto` will
+        /// override the padding set by this widget
         pub padding_left: StyleProp<Units>,
+        /// The inner padding between the right edge of this widget and its children
+        ///
+        /// A child with their own `right` property set to anything other than `Units::Auto` will
+        /// override the padding set by this widget
         pub padding_right: StyleProp<Units>,
+        /// The inner padding between the top edge of this widget and its children
+        ///
+        /// A child with their own `top` property set to anything other than `Units::Auto` will
+        /// override the padding set by this widget
         pub padding_top: StyleProp<Units>,
+        /// Controls how the pointer interacts with the widget
+        ///
+        /// This can be used to block pointer events on itself and/or its children if needed, allowing
+        /// the event to "pass through" to widgets below.
         pub pointer_events: StyleProp<PointerEvents>,
+        /// The position type of the widget relative to its parent
         pub position_type: StyleProp<PositionType>,
+        /// The render method for this widget
+        ///
+        /// This controls what actually gets rendered and how it's rendered.
         pub render_command: StyleProp<RenderCommand>,
+        /// The distance between the right edge of this widget and the right edge of its containing widget
         pub right: StyleProp<Units>,
+        /// The distance between the top edge of this widget and the top edge of its containing widget
         pub top: StyleProp<Units>,
+        /// The width of this widget
         pub width: StyleProp<Units>,
     }
 }
@@ -232,5 +346,155 @@ impl AsRefOption<Style> for Option<Style> {
 impl AsRefOption<Style> for &Option<Style> {
     fn as_ref_option(&self) -> Option<&Style> {
         self.as_ref()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{Style, StyleProp, Units};
+
+    #[test]
+    fn styles_should_equal() {
+        let mut a = Style::default();
+        let mut b = Style::default();
+        assert_eq!(a, b);
+
+        a.height = StyleProp::Default;
+        b.height = StyleProp::Inherit;
+        assert_ne!(a, b);
+    }
+
+    #[test]
+    fn style_should_inherit_property() {
+        let border = (1.0, 2.0, 3.0, 4.0);
+
+        let parent = Style {
+            border: StyleProp::Value(border),
+            ..Default::default()
+        };
+        let mut child = Style {
+            border: StyleProp::Inherit,
+            ..Default::default()
+        };
+
+        child.inherit(&parent);
+
+        assert_eq!(border, child.border.resolve());
+    }
+
+    #[test]
+    #[should_panic]
+    fn style_should_panic_on_resolve_inherit_property() {
+        let style = Style {
+            color: StyleProp::Inherit,
+            ..Default::default()
+        };
+
+        let _ = style.color.resolve();
+    }
+
+    #[test]
+    fn style_should_apply_styles_on_unset_property() {
+        let mut base_style = Style::default();
+        let other_style = Style {
+            width: StyleProp::Value(Units::Pixels(123.0)),
+            ..Default::default()
+        };
+
+        assert_ne!(base_style.width, other_style.width);
+
+        base_style.apply(&other_style);
+
+        assert_eq!(base_style.width, other_style.width);
+    }
+
+    #[test]
+    fn style_should_not_apply_styles_on_non_unset_property() {
+        let mut base_style = Style {
+            width: StyleProp::Default,
+            ..Default::default()
+        };
+        let other_style = Style {
+            width: StyleProp::Value(Units::Pixels(123.0)),
+            ..Default::default()
+        };
+
+        assert_ne!(base_style.width, other_style.width);
+
+        base_style.apply(&other_style);
+
+        assert_ne!(base_style.width, other_style.width);
+    }
+
+    #[test]
+    fn style_should_apply_option_style() {
+        let mut base_style = Style::default();
+        let other_style = Some(Style {
+            width: StyleProp::Value(Units::Pixels(123.0)),
+            ..Default::default()
+        });
+
+        assert_ne!(base_style.width, other_style.as_ref().unwrap().width);
+
+        base_style.apply(&other_style);
+
+        assert_eq!(base_style.width, other_style.as_ref().unwrap().width);
+
+        base_style.apply(None);
+
+        assert_eq!(base_style.width, other_style.as_ref().unwrap().width);
+    }
+
+    #[test]
+    fn style_should_not_apply_none() {
+        let expected = Style::default();
+        let mut base_style = expected.clone();
+
+        assert_eq!(expected, base_style);
+        base_style.apply(None);
+        assert_eq!(expected, base_style);
+    }
+
+    #[test]
+    fn styles_should_be_buildable() {
+        let expected_left = StyleProp::Default;
+        let expected_width = StyleProp::Value(Units::Stretch(1.0));
+        let expected_height = StyleProp::Inherit;
+
+        let expected = Style {
+            left: expected_left.clone(),
+            width: expected_width.clone(),
+            height: expected_height.clone(),
+            ..Default::default()
+        };
+
+        let style = Style::default()
+            // Pass ownership
+            .with_style(Style {
+                height: expected_height,
+                ..Default::default()
+            })
+            // Pass ownership of option
+            .with_style(Some(Style {
+                left: expected_left,
+                ..Default::default()
+            }))
+            // Pass reference
+            .with_style(&Style {
+                width: expected_width,
+                ..Default::default()
+            });
+
+        assert_eq!(expected, style);
+    }
+
+    #[test]
+    fn value_should_convert_to_property() {
+        let expected_width = Units::Pixels(123.0);
+        let expected = StyleProp::Value(expected_width.clone());
+
+        let property: StyleProp<_> = expected_width.into();
+
+        assert_eq!(expected, property);
     }
 }

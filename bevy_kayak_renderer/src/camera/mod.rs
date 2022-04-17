030207d246d94e8cd3683e8497fa26c1f56a3cdf
Christopher Biscardi <chris@christopherbiscardi.com>
2022-04-17 08:07:59 -0700

    missed a comment removal

--- bevy_kayak_renderer/src/camera/mod.rs
+++ bevy_kayak_renderer/src/camera/mod.rs
@@ -13,8 +13,6 @@ pub struct KayakUICameraPlugin;
 
 impl Plugin for KayakUICameraPlugin {
     fn build(&self, app: &mut bevy::prelude::App) {
-        // let mut active_cameras = app.world.get_resource_mut::<ActiveCameras>().unwrap();
-        // active_cameras.add(UICameraBundle::UI_CAMERA);
         app.add_system_to_stage(
             CoreStage::PostUpdate,
             bevy::render::camera::camera_system::<UIOrthographicProjection>,

ceb69da84d7e85480c28f9fbc6d0fb353d6b47b5
Louis Capitanchik <contact@louiscap.co>
2022-08-19 01:03:06 +0100

    Update min_binding_size to match byte layout

--- bevy_kayak_renderer/src/render/unified/pipeline.rs
+++ bevy_kayak_renderer/src/render/unified/pipeline.rs
@@ -96,7 +96,7 @@ impl FromWorld for UnifiedPipeline {
                     has_dynamic_offset: true,
                     // TODO: change this to ViewUniform::std140_size_static once crevice fixes this!
                     // Context: https://github.com/LPGhatguy/crevice/issues/29
-                    min_binding_size: BufferSize::new(4),
+                    min_binding_size: BufferSize::new(16),
                 },
                 count: None,
             }],

89da2dddeb006180a8a4f0155ac72bf091b0b8c0
John Mitchell <startoaster23@gmail.com>
2022-11-15 09:01:07 -0500

    Fix bug where root node could accidentally be removed.

--- src/tree.rs
+++ src/tree.rs
@@ -77,7 +77,11 @@ impl Tree {
             children
         } else {
             // Is root node
-            self.root_node = None;
+            if let Some(root_node) = self.root_node {
+                if root_node == index {
+                    self.root_node = None;
+                }
+            }
             Vec::default()
         }
     }

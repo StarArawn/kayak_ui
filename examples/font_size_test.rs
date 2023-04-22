d5288d68bf3cf249cfd29884d14714dec7db370e
John Mitchell <6656977+StarArawn@users.noreply.github.com>
2023-04-21 22:50:26 -0400

    Various fixes to how we handle font rendering.

---
+++ examples/font_size_test.rs
@@ -0,0 +1,61 @@
+use bevy::prelude::*;
+use kayak_ui::{
+    prelude::{widgets::*, *},
+    CameraUIKayak,
+};
+
+fn startup(
+    mut commands: Commands,
+    mut font_mapping: ResMut<FontMapping>,
+    asset_server: Res<AssetServer>,
+) {
+    let camera_entity = commands
+        .spawn(Camera2dBundle::default())
+        .insert(CameraUIKayak)
+        .id();
+
+    font_mapping.set_default(asset_server.load("fonts/roboto.kttf"));
+    font_mapping.force_subpixel(&asset_server.load("fonts/roboto.kttf"));
+
+    let mut widget_context = KayakRootContext::new(camera_entity);
+    widget_context.add_plugin(KayakWidgetsContextPlugin);
+    let parent_id = None;
+    rsx! {
+        <KayakAppBundle>
+            <TransitionBundle
+                transition={TransitionProps {
+                    easing: TransitionEasing::SineInOut,
+                    timeout: 5000.0,
+                    looping: true,
+                    style_a: KStyle {
+                        font_size: 10.0.into(),
+                        ..Default::default()
+                    },
+                    style_b: KStyle {
+                        font_size: 1000.0.into(),
+                        ..Default::default()
+                    },
+                    ..Default::default()
+                }}
+            >
+                <TextWidgetBundle
+                    text={TextProps {
+                        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sed tellus neque. Proin tempus ligula a mi molestie aliquam.".into(),
+                        ..Default::default()
+                    }}
+                />
+            </TransitionBundle>
+        </KayakAppBundle>
+    };
+
+    commands.spawn((widget_context, EventDispatcher::default()));
+}
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugin(KayakContextPlugin)
+        .add_plugin(KayakWidgets)
+        .add_startup_system(startup)
+        .run()
+}
